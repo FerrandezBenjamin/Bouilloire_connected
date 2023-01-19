0b5d2c22be58a4a1a9ece3a1231e50ae52b0dd60
FerrandezBenjamin <58741067+FerrandezBenjamin@users.noreply.github.com>
2023-01-19 10:58:35 +0100

    les led sallumes en fonction de la temperature

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,79 @@
 #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 const int LEDJAUNE = 5;
 const int LEDROUGE = 4;
 const int LEDVERTE = 0;
 const int LEDBLEU = 12;
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 14;
+const int BAUDRATE = 115200;
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+DallasTemperature sensors(&oneWire);
+
+void startled(float temp)
+{
+  if (temp >= 20 && temp < 25)
+  {
+    // start led bleu -- eteindre les autres
+    digitalWrite(LEDBLEU, HIGH);
+    // eteindre les autres
+    digitalWrite(LEDJAUNE, LOW);
+    digitalWrite(LEDROUGE, LOW);
+    digitalWrite(LEDVERTE, LOW);
+  }
+  else if (temp >= 25 && temp < 30)
+  {
+    // start vert -- eteindre les autres
+    digitalWrite(LEDVERTE, HIGH);
+    // eteindre les autres
+    digitalWrite(LEDROUGE, LOW);
+    digitalWrite(LEDBLEU, LOW);
+    digitalWrite(LEDJAUNE, LOW);
+  }
+  else if (temp >= 30 && temp < 35)
+  {
+    // start jaune -- eteindre les autres
+    digitalWrite(LEDJAUNE, HIGH);
+    // eteindre les autres
+    digitalWrite(LEDROUGE, LOW);
+    digitalWrite(LEDVERTE, LOW);
+    digitalWrite(LEDBLEU, LOW);
+  }
+  else if (temp >= 35 && temp < 40)
+  {
+    // start red -- eteindre les autres
+    digitalWrite(LEDROUGE, HIGH);
+    // eteindre les autres
+    digitalWrite(LEDVERTE, LOW);
+    digitalWrite(LEDBLEU, LOW);
+    digitalWrite(LEDJAUNE, LOW);
+  }
+  else
+  {
+    // start all led pour les faires clignoter
+    digitalWrite(LEDROUGE, HIGH);
+    digitalWrite(LEDVERTE, HIGH);
+    digitalWrite(LEDBLEU, HIGH);
+    digitalWrite(LEDJAUNE, HIGH);
+
+    delay(250);
+
+    digitalWrite(LEDROUGE, LOW);
+    digitalWrite(LEDVERTE, LOW);
+    digitalWrite(LEDBLEU, LOW);
+    digitalWrite(LEDJAUNE, LOW);
+    delay(250);
+  }
+}
 
 void setup()
 {
+  Serial.begin(BAUDRATE);
+  sensors.begin();
   pinMode(LEDJAUNE, OUTPUT);
   pinMode(LEDROUGE, OUTPUT);
   pinMode(LEDVERTE, OUTPUT);
@@ -16,18 +83,11 @@ void setup()
 
 void loop()
 {
-  digitalWrite(LEDJAUNE, HIGH);
-  digitalWrite(LEDROUGE, HIGH);
-  digitalWrite(LEDVERTE, HIGH);
-  digitalWrite(LEDBLEU, HIGH);
+  sensors.requestTemperatures();
+  float temperatureC = sensors.getTempCByIndex(0);
+  Serial.print(temperatureC);
+  Serial.println("ºC");
 
-  delay(250);
-
-  digitalWrite(LEDJAUNE, LOW);
-  digitalWrite(LEDROUGE, LOW);
-  digitalWrite(LEDVERTE, LOW);
-  digitalWrite(LEDBLEU, LOW);
-
-  delay(250);
+  startled(temperatureC);
   // put your main code here, to run repeatedly:
 }
