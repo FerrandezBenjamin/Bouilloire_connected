691e5df21322c80f334fba82e03d64ece2473a69
FerrandezBenjamin <58741067+FerrandezBenjamin@users.noreply.github.com>
2023-01-19 09:06:41 +0100

    first dev

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
 #include <Arduino.h>
 
-void setup() {
+const int LEDJAUNE = 5;
+const int LEDROUGE = 4;
+const int LEDVERTE = 0;
+const int LEDBLEU = 12;
+
+void setup()
+{
+  pinMode(LEDJAUNE, OUTPUT);
+  pinMode(LEDROUGE, OUTPUT);
+  pinMode(LEDVERTE, OUTPUT);
+  pinMode(LEDBLEU, OUTPUT);
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
+  digitalWrite(LEDJAUNE, HIGH);
+  digitalWrite(LEDROUGE, HIGH);
+  digitalWrite(LEDVERTE, HIGH);
+  digitalWrite(LEDBLEU, HIGH);
+
+  delay(250);
+
+  digitalWrite(LEDJAUNE, LOW);
+  digitalWrite(LEDROUGE, LOW);
+  digitalWrite(LEDVERTE, LOW);
+  digitalWrite(LEDBLEU, LOW);
+
+  delay(250);
   // put your main code here, to run repeatedly:
 }
